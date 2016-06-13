1141fa7edb08ddf1ca0b5d5eb0b02b5d99d15f66
John Bannister <jdbann@gmail.com>
2016-06-13 11:52:50 +0100

    Install fonts and prepare terminal themes
    Installs mononoki font automatically with post-up hook script closing #2.
    Opens `terminal themes` directory on post-up for installation by user.

---
+++ hooks/post-up/install-resources.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install fonts to user's fonts directory
+cp -n -R resources/fonts/ ~/Library/Fonts/
+
+# Open the terminal themes folder for installation
+open resources/terminal\ themes
