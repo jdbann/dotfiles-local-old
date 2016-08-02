bae40901077e0c0e58943aea727ca064716ba5ae
John Bannister <jdbann@gmail.com>
2016-08-02 09:41:14 +0100

    Better installation of terminal themes

--- hooks/post-up/install-resources.sh
+++ hooks/post-up/install-resources.sh
@@ -3,5 +3,11 @@
 # Install fonts to user's fonts directory
 cp -n -R resources/fonts/ ~/Library/Fonts/
 
-# Open the terminal themes folder for installation
-open resources/terminal\ themes
+# Check if solarized themes are installed
+_solarized=$(grep -U -i 'solarized' ~/Library/Preferences/com.apple.Terminal.plist)
+
+# If not found...
+if [ -z "$_solarized" ]; then
+  # Open the terminal themes folder for installation
+  open resources/terminal\ themes
+fi
